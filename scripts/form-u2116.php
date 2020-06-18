411376f2afd3b4a4f96148ead04a152e3b293d93
tomknows <58662618+tomknows@users.noreply.github.com>
2020-06-18 20:32:49 +0100

    Thu

--- scripts/form-u2116.php
+++ scripts/form-u2116.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => '',
-		'to' => ''
+		'from' => 'funerrltd@gmail.com',
+		'to' => 'funerrltd@gmail.com'
 	),
 	'fields' => array(
 		'custom_U2118' => array(
